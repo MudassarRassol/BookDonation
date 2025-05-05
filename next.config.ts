32c2fc24435ad8e63a15a2b773b318976c37e49b
Mudassar Rasool <mudassardev16@gmail.com>
2025-05-05 19:59:40 +0500

    updare

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   // /* config options here */
+//   images: {
+//     domains: ["res.cloudinary.com"], // Allow images from Cloudinary
+//   },
+// };
+
+// export default nextConfig;
+
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // /* config options here */
   images: {
-    domains: ["res.cloudinary.com"], // Allow images from Cloudinary
+    domains: ["res.cloudinary.com"],
   },
 };
 
 export default nextConfig;
+
