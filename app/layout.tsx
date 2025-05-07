702a8898d8caa015532ef4aceaa927052877611c
Mudassar Rasool <mudassardev16@gmail.com>
2025-05-07 22:33:54 +0500

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ReduxProvider from "@/app/redux/provider";
 import "./globals.css";
-// import '@ant-design/v5-patch-for-react-19';
+import '@ant-design/v5-patch-for-react-19';
 import LayoutWrapper from "@/components/LayoutWrapper";
 const geistSans = Geist({
   variable: "--font-geist-sans",
