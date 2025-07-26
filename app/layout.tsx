ba6e95bef3e8931fa348268489bc33b7d1935c28
MudassarRassol <mudassardev16@gmail.com>
2025-07-26 09:58:50 +0500

    add insght

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import ReduxProvider from "@/app/redux/provider";
 import "./globals.css";
 import '@ant-design/v5-patch-for-react-19';
 import LayoutWrapper from "@/components/LayoutWrapper";
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +25,7 @@ export const metadata : Metadata= {
     siteName: "Book Aid",
     images: [
       {
-        url: "https://book-donation-orcin.vercel.app/og-image.png",
+        url: "https://book-donation-orcin.vercel.app/globe.svg",
         width: 1200,
         height: 630,
         alt: "Book Aid",
@@ -37,7 +37,7 @@ export const metadata : Metadata= {
     card: "summary_large_image",
     title: "Book Aid",
     description: "A community-driven book donation platform.",
-    images: ["https://book-donation-orcin.vercel.app/og-image.png"],
+    images: ["https://book-donation-orcin.vercel.app/globe.svg"],
   },
 };
 
@@ -53,6 +53,7 @@ export default function RootLayout({
         <ReduxProvider>
           <LayoutWrapper>
             {children}
+            <SpeedInsights />
           </LayoutWrapper>
         </ReduxProvider>
       </body>
